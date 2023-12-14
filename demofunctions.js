42f190ac886b8558e5e75dbd1b6f38dacef23df8
ManassesGikwa <manassesgikwa@gmail.com>
2023-12-14 12:45:07 +0300

    completed ssignment

---
+++ demofunctions.js
@@ -0,0 +1,61 @@
+// function myfunctionName(){
+//     console.log("This function has been called")
+//     return ("Iam the one been returned")
+// }
+// myfunctionName()
+// let funcOutput = myfunctionName()
+// console.log(funcOutput)
+
+// //parameters(placeholders for arguments) + arguments(varying data in our functions )
+
+// //add
+
+// function addition(param1,param2){
+//     //take two numbers and return the same
+
+//     let mysum=(param1+param2)
+//     return mysum
+// }
+// console.log(addition(75,67))
+
+// //function Expressions
+// const adder= function(params){
+
+// }
+// adder(args);
+
+// const numAdders=function(num1,num2){
+//     let summation= num1 +num2
+//     return summation
+// }
+
+// console.log(numAdders(34,23))
+// console.log(numAdders(3111,277443))
+// console.log(numAdders(40000000,212343))
+// console.log(numAdders(3213456789,232345))
+
+
+//Arrow Functions
+
+// ()=>{}
+
+// (num1,num2)=>{
+//     let summation =num1+num2
+//     return summation
+// }
+
+// (num1,num2)=>num1+num2
+
+const mysum = (num1,num2)=>num1+num2
+
+let myName = "Manasses"
+
+function randomfunc(){
+    let myHiddenName="Gikwa";
+    return myHiddenName
+}
+console.log(myHiddenName)
+
+function randomfunc2(){
+    
+}
